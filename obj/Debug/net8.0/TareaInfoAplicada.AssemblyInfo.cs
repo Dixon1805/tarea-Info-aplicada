b92f4bcfb585fbbf77637beb98f9968ca303b0da
Dixon <dixonmunosmurillo1@gmail.com>
2024-10-23 00:44:23 -0600

    subir

--- obj/Debug/net8.0/TareaInfoAplicada.AssemblyInfo.cs
+++ obj/Debug/net8.0/TareaInfoAplicada.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c6278c7d72ded69e21417d7f101a1e23a035c9")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
