cd8cace7217a19978e4de9eabe6532b947e70948
Dixon <dixonmunosmurillo1@gmail.com>
2024-10-23 00:44:07 -0600

    subir a master

--- obj/Debug/net8.0/TareaInfoAplicada.AssemblyInfo.cs
+++ obj/Debug/net8.0/TareaInfoAplicada.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0cedd445c24cfc928cdffec2890d0ffcc1c43b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c6278c7d72ded69e21417d7f101a1e23a035c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaInfoAplicada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
